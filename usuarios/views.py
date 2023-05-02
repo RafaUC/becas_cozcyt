6d08383cd495b09b2fb448115c40d4473d7d4f9e
EsmeraldaPachecoGlez <esmeraldapacheco05@gmail.com>
2023-05-02 10:23:05 -0600

    Codificación funcionalidad registro
    Se realizó la codificación para que el usuario pueda crear una cuenta, una vez hecho esto, su registro aparece en la página del admin.

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+from .forms import CreateUserForm
+from .models import *
+from django.urls import reverse_lazy
+
 
 def login(request):
     if request.method == "POST":
@@ -10,6 +16,14 @@ def login(request):
     return render(request, "login.html")
 
 def register(request):
-    if request.method == "POST":
-        pass
-    return render(request, 'register.html')
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(data=request.POST)
+        if form.is_valid():
+            print('valid')
+            form.save()
+            return render(request, "login.html") #cambiar a la vista de confirmar email cuando se tenga implementada
+        else:
+            print('not valid')
+    context = {'form' : form}
+    return render(request, 'register.html', context)
