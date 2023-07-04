5b8bac3632b91bc0d21cab33dc1b6d5103dc7276
RafaUC <35164744@uaz.edu.mx>
2023-07-03 21:21:29 -0600

    agregados modelos al admin site

--- estudio_socio_economico/admin.py
+++ estudio_socio_economico/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Seccion, Opcion, Respuesta, RTextoCorto, RTextoParrafo, ROpcionMultiple, RCasillas, RDesplegable, Elemento, ElementoSeparador, ElementoTextoCorto, ElementoTextoParrafo, ElementoOpcionMultiple, ElementoCasillas, ElementoDesplegable
 # Register your models here.
+
+admin.site.register(Seccion)
+admin.site.register(Opcion)
+admin.site.register(Respuesta)
+admin.site.register(RTextoCorto)
+admin.site.register(RTextoParrafo)
+admin.site.register(ROpcionMultiple)
+admin.site.register(RCasillas)
+admin.site.register(RDesplegable)
+admin.site.register(Elemento)
+admin.site.register(ElementoSeparador)
+admin.site.register(ElementoTextoCorto)
+admin.site.register(ElementoTextoParrafo)
+admin.site.register(ElementoOpcionMultiple)
+admin.site.register(ElementoCasillas)
+admin.site.register(ElementoDesplegable)
