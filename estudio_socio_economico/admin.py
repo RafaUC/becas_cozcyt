ee17a45b72a3e030ab67b42a1e53909b4fcdb23b
RafaUC <35164744@uaz.edu.mx>
2023-07-11 17:57:12 -0600

    Eleminados modelos inecesarios de Elementos

--- estudio_socio_economico/admin.py
+++ estudio_socio_economico/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Seccion, Opcion, Respuesta, RTextoCorto, RTextoParrafo, ROpcionMultiple, RCasillas, RDesplegable, Elemento, ElementoSeparador, ElementoTextoCorto, ElementoTextoParrafo, ElementoOpcionMultiple, ElementoCasillas, ElementoDesplegable
+from .models import Seccion, Opcion, Respuesta, RTextoCorto, RTextoParrafo, ROpcionMultiple, RCasillas, RDesplegable, Elemento
 # Register your models here.
 
 admin.site.register(Seccion)
@@ -11,9 +11,3 @@ admin.site.register(ROpcionMultiple)
 admin.site.register(RCasillas)
 admin.site.register(RDesplegable)
 admin.site.register(Elemento)
-admin.site.register(ElementoSeparador)
-admin.site.register(ElementoTextoCorto)
-admin.site.register(ElementoTextoParrafo)
-admin.site.register(ElementoOpcionMultiple)
-admin.site.register(ElementoCasillas)
-admin.site.register(ElementoDesplegable)
