f7d167b07d2a5321aa214514c38005b7952406b0
RafaUC <35164744@uaz.edu.mx>
2024-01-23 02:12:22 -0600

    implementado la generacion y descarga del concentrado

--- solicitudes/viewsAdmin.py
+++ solicitudes/viewsAdmin.py
@@ -13,6 +13,9 @@ from django.conf import settings
 import colorsys
 from django.db.models import Count
 from zipfile import ZipFile
+import uuid
+from openpyxl import Workbook
+from datetime import date
 
 from usuarios.views import verificarRedirect
 from usuarios.viewsAdmin import BusquedaEnCamposQuerySet
@@ -374,7 +377,7 @@ def documentos_solicitante(request, pk):
     return render(request, 'admin/documentosSolicitante.html', context)
 
 @login_required
-def concentradoSolicitante(request,pk):
+def concentradoSolicitud(request,pk):
     usuario = get_object_or_404(Usuario, pk=request.user.id)  
     url = verificarRedirect(usuario, 'permiso_administrador')    
     if url:          #Verifica si el usuario ha llenaodo su informacion personal por primera vez y tiene los permisos necesarios
@@ -384,16 +387,394 @@ def concentradoSolicitante(request,pk):
     if not os.path.exists(temp_dir):# Si no existe, lo crea
         os.makedirs(temp_dir)
 
+    uid = str(uuid.uuid4())
+    uid = f'{uid}.zip'
     solicitud = get_object_or_404(Solicitud, pk=pk)
     documentosResp = RespuestaDocumento.objects.filter(solicitud=solicitud)    
-    zip_filename = f'concentrado-{solicitud.solicitante.folio}-{ciclo_actual()}.zip'
+    zip_filename = f'concentrado-{solicitud.solicitante.folio}-{solicitud.modalidad.nombre.replace(" ","_")}-{solicitud.modalidad.tipo}-{solicitud.ciclo}.zip'
     zip_filename = zip_filename.replace(' ','')
-    with ZipFile(temp_dir + zip_filename, 'w') as zipObj:
+    with ZipFile(temp_dir + uid, 'w') as zipObj:
         for resp in documentosResp:
             originalName = os.path.basename(resp.file.name)
             extension = os.path.splitext(originalName)[1]
             filename = os.path.basename(resp.documento.nombre) + extension            
             zipObj.write(resp.file.path, arcname=filename)
-    response = FileResponse(open(temp_dir+zip_filename, 'rb'), as_attachment=True)
-    os.remove(temp_dir+zip_filename) # Elimina el archivo temporal
+    response = FileResponse(open(temp_dir+uid, 'rb'), as_attachment=True, filename=zip_filename)
+    os.remove(temp_dir+uid) # Elimina el archivo temporal
     return response
+
+@login_required
+def concentradoConvocatoria(request,ciclo):
+    usuario = get_object_or_404(Usuario, pk=request.user.id)  
+    url = verificarRedirect(usuario, 'permiso_administrador')    
+    if url:          #Verifica si el usuario ha llenaodo su informacion personal por primera vez y tiene los permisos necesarios
+        return redirect(url)
+    
+    temp_dir =  os.path.join(settings.BASE_DIR,'temp/')     
+    if not os.path.exists(temp_dir):# Si no existe, lo crea
+        os.makedirs(temp_dir)
+        
+    documentosResp = RespuestaDocumento.objects.filter(solicitud__ciclo=ciclo).select_related(
+    'solicitud',
+    'documento',
+    'documento__modalidad',
+    'solicitud__modalidad',
+    'solicitud__solicitante',    
+    )
+    uid = str(uuid.uuid4())
+    uid = f'{uid}.zip'
+    zip_filename = f'CONCENTRADO-{ciclo}.zip'
+    zip_filename = zip_filename.replace(' ','')
+
+    xlPath = generarConcentradoXlsx(ciclo)
+
+    with ZipFile(temp_dir + uid, 'w') as zipObj:
+        zipObj.write(xlPath, arcname=(f'CONCENTRADO-{ciclo}.xlsx').replace(' ',''))
+
+        total_solicitudes = len(documentosResp)
+        for indice, resp in enumerate(documentosResp, start=1):
+            print(f'generando concentrado docs: {(indice / total_solicitudes) * 100:.2f}%')        
+            originalName = os.path.basename(resp.file.name)
+            extension = os.path.splitext(originalName)[1]
+            filename = os.path.basename(resp.documento.nombre) + extension    
+
+            relative_path = os.path.join(
+                f'{resp.solicitud.modalidad.nombre}-{resp.solicitud.modalidad.tipo}',
+                resp.solicitud.solicitante.folio,
+                filename
+            )
+            zipObj.write(resp.file.path, arcname=relative_path)
+
+    response = FileResponse(open(temp_dir+uid, 'rb'), as_attachment=True, filename=zip_filename)
+    os.remove(temp_dir+uid) # Elimina el archivo temporal
+    os.remove(xlPath) # Elimina el archivo temporal
+    return response
+
+def generarConcentradoXlsx(ciclo):
+    temp_dir =  os.path.join(settings.BASE_DIR,'temp/')     
+    if not os.path.exists(temp_dir):# Si no existe, lo crea
+        os.makedirs(temp_dir)
+    uid = str(uuid.uuid4())
+    uid = f'{uid}.xlsx'
+    path = temp_dir + uid
+
+    wb = Workbook()    
+    ws = wb.active
+
+    solicitudes = Solicitud.objects.filter(ciclo=ciclo).select_related(            
+    'modalidad',
+    'solicitante__municipio',    
+    'solicitante__municipio__estado',
+    'solicitante__carrera',
+    'solicitante__carrera__institucion'
+    )
+    ws.append([
+        'Estatus',
+        'Estado_Beca',
+        'Modalidad',
+        'Folio',
+        'Nombre',
+        'Ap_Paterno',
+        'Ap_Materno',
+        'RFC',
+        'Genero',
+        'Punt_Genero',
+        'Edad',
+        'CURP',
+        'Calle_y_Num',
+        'CP',
+        'Colonia',
+        'Municipio',
+        'Punt_Municipio',
+        'Entidad_Federativa',
+        'Tel_Casa',
+        'Tel_Cel',
+        'Correo',
+        'Institución',
+        'Punt_Institución',
+        'Carrera',
+        'Punt_Carrera',
+        'Periodo',
+        'Punt_Periodo',
+        'Promedio',
+        'Punt_Promedio',
+        'Tipo_Solicitud',
+        'Punt_Tipo_Solicitud',
+        'Ingresos',
+        'Punt_Ingresos',        
+        'Total',
+    ])
+    seccionChoices = PuntajeGeneral.SECCION_CHOICES    
+    puntajesGenero = PuntajeGeneral.objects.filter(tipo = seccionChoices[0][0])  
+    puntajesPeriodo = PuntajeGeneral.objects.filter(tipo = seccionChoices[3][0])  
+    puntajesPromedio = PuntajeGeneral.objects.filter(tipo = seccionChoices[4][0])     
+    puntajeRenovacion = PuntajeGeneral.objects.get(tipo = seccionChoices[2][0], nombre='Renovación')                
+    puntajeIngreso = PuntajeGeneral.objects.get(tipo = seccionChoices[2][0], nombre='Nuevo ingreso')                
+    puntajesIngresosM = PuntajeGeneral.objects.filter(tipo = seccionChoices[1][0])
+    q = Q(**{'nombre__icontains': 'Sueldo mensual familiar'}) 
+    ingresoPreguntas = Elemento.objects.filter(tipo=Elemento.TIPO_CHOICES[1][0]).filter(q)   
+    ids_preguntas = ingresoPreguntas.values_list('id', flat=True)
+
+    total_solicitudes = len(solicitudes)
+    for indice, solicitud in enumerate(solicitudes, start=1):
+        print(f'generando concentrado xlsx: {(indice / total_solicitudes) * 100:.2f}%')
+        values = []
+        nuevoPuntaje = 0
+        #Estatus
+        try:
+            documentos_modalidad = solicitud.modalidad.documento_set.all()
+            respuestas_documentos_solicitud = RespuestaDocumento.objects.filter(solicitud=solicitud) 
+            if set(documentos_modalidad) == set({resp.documento for resp in respuestas_documentos_solicitud}):        
+                values.append('Completo')
+            else:
+                values.append('Incompleto')
+        except Exception as e:
+            values.append(None)
+
+        #Estado_Beca
+        try:
+            values.append(solicitud.estado)
+        except Exception as e:
+            values.append(None)
+
+        #Modalidad
+        try:
+            values.append(solicitud.modalidad.nombre)
+        except Exception as e:
+            values.append(None)
+
+        #Folio
+        try:
+            values.append(solicitud.solicitante.folio)
+        except Exception as e:
+            values.append(None)
+
+        #Nombre
+        try:
+            values.append(solicitud.solicitante.nombre)
+        except Exception as e:
+            values.append(None)
+
+        #Ap_Paterno
+        try:
+            values.append(solicitud.solicitante.ap_paterno)
+        except Exception as e:
+            values.append(None)
+
+        #Ap_Materno
+        try:
+            values.append(solicitud.solicitante.ap_materno)
+        except Exception as e:
+            values.append(None)
+
+        #rfc
+        try:
+            values.append(solicitud.solicitante.rfc)
+        except Exception as e:
+            values.append(None)
+
+        #genero
+        try:
+            values.append(solicitud.solicitante.genero)
+        except Exception as e:
+            values.append(None)
+
+        #Punt_genero
+        try:            
+            for puntaje in puntajesGenero:
+                if solicitud.solicitante.genero == puntaje.nombre:
+                    nuevoPuntaje += puntaje.puntos
+                    values.append(puntaje.puntos)
+                    break
+                elif puntaje.id == puntajesGenero.last().id:
+                    values.append(None)
+        except Exception as e:
+            values.append(None)
+
+        #Edad
+        try:
+            hoy = date.today()
+            fecha_nacimiento = solicitud.solicitante.fecha_nacimiento
+            edad = hoy.year - fecha_nacimiento.year - ((hoy.month, hoy.day) < (fecha_nacimiento.month, fecha_nacimiento.day))
+            values.append(edad)
+        except Exception as e:
+            values.append(None)
+
+        #CURP
+        try:
+            values.append(solicitud.solicitante.curp)
+        except Exception as e:
+            values.append(None)
+
+        #Calle_y_Num
+        try:
+            values.append(f'{solicitud.solicitante.calle} {solicitud.solicitante.numero}')
+        except Exception as e:
+            values.append(None)
+
+        #CP
+        try:
+            values.append(solicitud.solicitante.codigo_postal)
+        except Exception as e:
+            values.append(None)
+        #Colonia
+        try:
+            values.append(solicitud.solicitante.colonia)
+        except Exception as e:
+            values.append(None)
+
+        #Municipio
+        try:
+            values.append(solicitud.solicitante.municipio.nombre)
+        except Exception as e:
+            values.append(None)
+
+        #Punt_Municipio
+        try:
+            puntajeMun = PuntajeMunicipio.objects.get(municipio=solicitud.solicitante.municipio_id)
+            nuevoPuntaje += puntajeMun.puntos
+            values.append(puntajeMun.puntos)
+        except PuntajeMunicipio.DoesNotExist:
+            values.append(0)
+        except Exception as e:
+            values.append(None)
+
+        #Entidad_Federativa
+        try:
+            values.append(solicitud.solicitante.municipio.estado.nombre)
+        except Exception as e:
+            values.append(None)
+
+        #Tel_Casa
+        try:
+            values.append(solicitud.solicitante.tel_fijo)
+        except Exception as e:
+            values.append(None)
+
+        #Tel_Cel
+        try:
+            values.append(solicitud.solicitante.tel_cel)
+        except Exception as e:
+            values.append(None)
+
+        #Correo
+        try:
+            values.append(solicitud.solicitante.email)
+        except Exception as e:
+            values.append(None)
+
+        #Institución
+        try:
+            values.append(solicitud.solicitante.carrera.institucion.nombre)
+        except Exception as e:
+            values.append(None)
+
+        #Punt_Institución
+        try:
+            puntos = solicitud.solicitante.carrera.institucion.puntos
+            nuevoPuntaje += puntos
+            values.append(puntos)
+        except Exception as e:
+            values.append(None)
+
+        #Carrera
+        try:
+            values.append(solicitud.solicitante.carrera.nombre)
+        except Exception as e:
+            values.append(None)
+
+        #Punt_Carrera
+        try:
+            carrera = solicitud.solicitante.carrera
+            nuevoPuntaje += carrera.puntos
+            values.append(carrera.puntos)
+        except Exception as e:
+            values.append(None)
+
+        #Periodo
+        try:            
+            values.append(solicitud.solicitante.grado)
+        except Exception as e:
+            values.append(None)
+
+        #Punt_Periodo
+        try:
+            for puntaje in puntajesPeriodo:
+                limite_inferior, limite_superior = map(int, puntaje.nombre.split('-'))
+                if limite_inferior <= int(solicitud.solicitante.grado) <= limite_superior:
+                    nuevoPuntaje += puntaje.puntos
+                    values.append(puntaje.puntos)
+                    break
+                elif puntaje.id == puntajesPeriodo.last().id:
+                    values.append(None)
+        except Exception as e:
+            values.append(None)
+
+        #Promedio
+        try:
+            values.append(solicitud.solicitante.promedio)
+        except Exception as e:
+            values.append(None)
+
+        #Punt_Promedio
+        try:               
+            for puntaje in puntajesPromedio:
+                limite_inferior, limite_superior = map(float, puntaje.nombre.split('-'))            
+                if limite_inferior <= solicitud.solicitante.promedio <= limite_superior:
+                    nuevoPuntaje += puntaje.puntos
+                    values.append(puntaje.puntos)
+                    break
+                elif puntaje.id == puntajesPromedio.last().id:
+                    values.append(0)
+        except Exception as e:
+            values.append(None)
+
+        #Tipo_Solicitud
+        try:
+            values.append(solicitud.tipo)
+        except Exception as e:
+            values.append(None)
+
+        #Punt_Tipo_Solicitud
+        try:
+            if solicitud.solicitante.es_renovacion:                
+                nuevoPuntaje += puntajeRenovacion.puntos
+                values.append(puntajeRenovacion.puntos)
+            else:                
+                nuevoPuntaje += puntajeIngreso.puntos
+                values.append(puntajeIngreso.puntos)
+        except Exception as e:
+            values.append(None)        
+
+        #Ingresos
+        #Punt_Ingresos
+        try:            
+            ingresos = [] #lista de querysets relacionados con ingresos    
+            # Obtener todas las respuestas que pertenecen a las preguntas
+            respuestas = RNumerico.objects.filter(elemento__id__in=ids_preguntas, solicitante=solicitud.solicitante)
+            ingresos = respuestas.values_list('valor', flat=True)
+            ingresos = list(map(int, ingresos))        
+            ingresoTotal = sum(ingresos)   
+            values.append(ingresoTotal)
+            for puntaje in puntajesIngresosM:
+                limite_inferior, limite_superior = map(int, puntaje.nombre.replace('$', '').split('-'))
+                if limite_inferior <= ingresoTotal <= limite_superior:
+                    nuevoPuntaje += puntaje.puntos
+                    values.append(puntaje.puntos)
+                    break
+                elif puntaje.id == puntajesIngresosM.last().id:
+                    values.append(None)
+        except Exception as e:
+            values.append(None)        
+
+        #Total
+        try:
+            values.append(nuevoPuntaje)
+        except Exception as e:
+            values.append(None)
+
+        #print(values)
+        ws.append(values)
+
+    wb.save(path)
+    return path
