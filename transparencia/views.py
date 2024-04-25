4f243b2b070274011d1a0e6376c4c73bf94cdf00
EsmeraldaPachecoGlez <esmeraldapacheco05@gmail.com>
2024-04-24 23:01:14 -0600

    Interfaz inicio transparencia
    Se creó la interfaz de página de inicio de transparencia

--- transparencia/views.py
+++ transparencia/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from solicitudes.models import getListaCiclos
+from modalidades.models import ciclo_actual
 
 def getContextoBaseTransparencia(request):    
     ciclosPrevios = getListaCiclos()[:2]
@@ -11,9 +12,10 @@ def getContextoBaseTransparencia(request):
 
 # Create your views here.
 def inicioTransparencia(request):
-    
+    ciclo = ciclo_actual
     context = {
-
+    'ciclo': ciclo
     }
-    context.update(getContextoBaseTransparencia(request))
-    return render(request, 'base_transparencia.html', context)
+
+    context.update(getContextoBaseTransparencia(request)) #permi
+    return render(request, 'inicio.html', context)
