b72e4a535d41b14eadbd12ffad990a7f9db5272a
RafaUC <35164744@uaz.edu.mx>
2024-04-26 12:30:13 -0600

    implementada la funcion de que el administrador puede publicar los resultados de la convocatoria actual

--- transparencia/views.py
+++ transparencia/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from solicitudes.models import getListaCiclos
 from .forms import ModalidadSelectForm
-from modalidades.models import Modalidad
+from modalidades.models import Modalidad, Convocatoria, ciclo_actual
 from solicitudes.models import Solicitud
 
 
@@ -25,7 +25,7 @@ def inicioTransparencia(request):
 def resultadosValidarCiclo(request,num):
     #solo obtenemos los ultimos 2 ciclos
     ciclosPrevios = getListaCiclos()[:2]     
-    
+        
     ciclo = ciclosPrevios[0]    
     try:
         if num > 0:
@@ -46,18 +46,28 @@ def resultados(request, num):
 
 def resultadosContenido(request,num,mod):
     ciclo = resultadosValidarCiclo(request, num)    
-    modalidad = Modalidad.objects.filter(id=mod, mostrar=True).first()    
-    if not modalidad:
-        modalidad = Modalidad.objects.filter(mostrar=True).first()    
-    modalidadSelectForm = ModalidadSelectForm(initial={'modalidad': mod})
 
-    #obtener solicitudes aceptadas
-    solicitudes = Solicitud.objects.filter(ciclo=ciclo, modalidad=modalidad, estado=Solicitud.ESTADO_CHOICES[3][0]).select_related('solicitante').order_by('solicitante_id')
-    folios = [solicitud.solicitante.folio for solicitud in solicitudes]
+    convocatoria = Convocatoria.objects.all().first() #Obtiene la convocatoria    
+    cicloAct = ciclo_actual()
+    #si el ultimo siclo publicado no coincide con el actual y el seleccionado es el actual, siginifica que no esta publicado
+    if convocatoria.ultimo_ciclo_publicado != cicloAct and ciclo == cicloAct:
+        folios = []
+        titulo = "Los resultados de la convocatoria todavía no se han publicado."
+        modalidadSelectForm = None
+    else:
+        modalidad = Modalidad.objects.filter(id=mod, mostrar=True).first()    
+        if not modalidad:
+            modalidad = Modalidad.objects.filter(mostrar=True).first()    
+        modalidadSelectForm = ModalidadSelectForm(initial={'modalidad': mod})
+
+        #obtener solicitudes aceptadas
+        solicitudes = Solicitud.objects.filter(ciclo=ciclo, modalidad=modalidad, estado=Solicitud.ESTADO_CHOICES[3][0]).select_related('solicitante').order_by('solicitante_id')
+        folios = [solicitud.solicitante.folio for solicitud in solicitudes]
+        titulo = modalidad
 
     context = {
         'folios': folios,
-        'titulo': modalidad,
+        'titulo': titulo,
         'modalidadSelectForm': modalidadSelectForm,
     }
     return render(request, 'resultadosContenido.html', context)
