bcb8661bf98bee0fcbb71a6697bf857ed3d8b74f
EsmeraldaPachecoGlez <esmeraldapacheco05@gmail.com>
2024-04-26 00:06:01 -0600

    Inicio transparencia carrusel modalidades
    Se integró el carrusel donde se muestran las modalidades disponibles para la convocatoria.
    Falta responsividad.

--- transparencia/views.py
+++ transparencia/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from solicitudes.models import getListaCiclos
+from django.db.models import Count
+from django.db.models import Subquery, OuterRef
 
 from modalidades.forms import *
+from solicitudes.forms import *
+from solicitudes.models import *
 from modalidades.models import ciclo_actual
 
 
@@ -17,11 +21,14 @@ def getContextoBaseTransparencia(request):
 def inicioTransparencia(request):
     ciclo = ciclo_actual
     convocatoria = Convocatoria.objects.all().first()
+    modalidades = Modalidad.objects.filter(pk__in = Modalidad.objects.values('nombre').distinct().annotate(pk = Subquery(Modalidad.objects.filter(nombre= OuterRef("nombre")).order_by("pk").values("pk")[:1])).values_list("pk", flat=True))
     fecha_convocatoria = convocatoria.fecha_convocatoria if convocatoria else False
+    
     context = {
     'ciclo': ciclo,
     'convocatoria':convocatoria,
-    'fecha_convocatoria': fecha_convocatoria
+    'fecha_convocatoria': fecha_convocatoria,
+    'modalidades': modalidades
     }
 
     context.update(getContextoBaseTransparencia(request)) #permi
