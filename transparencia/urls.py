fd2ea0631ae890bbcedcd4623699bcfedc5e4f45
RafaUC <rafa_ruc6@hotmail.com>
2024-05-28 10:18:20 -0600

    actualizado la vista de beneficiados y la tabla

--- transparencia/urls.py
+++ transparencia/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views as auth_views
 app_name = 'transparencia'
 urlpatterns = [
     path('transparencia/', views.inicioTransparencia, name='Tinicio'),
-    path('transparencia/resultados/<int:num>', views.resultados, name='Tresultados'),
+    path('transparencia/resultados/<int:num>', views.resultados, name='TBeneficiarios'),
     path('transparencia/resultados/<int:num>/<int:mod>', views.resultadosContenido, name='TresCont'),
     path('transparencia/sit', views.transparenciaSIT, name='Tsit'),    
     path('transparencia/sitInfo', views.transparenciaSITInfo, name='TsitInfo'),
